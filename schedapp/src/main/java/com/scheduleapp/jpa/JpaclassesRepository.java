4e55a8a73794a6d5fecac7d45438bf8e877cd13b
lmleonardo <70298072+lmleonardo@users.noreply.github.com>
2020-11-07 22:24:35 -0500

    Add and edit classes

--- schedapp/src/main/java/com/scheduleapp/jpa/JpaclassesRepository.java
+++ schedapp/src/main/java/com/scheduleapp/jpa/JpaclassesRepository.java
@@ -10,14 +10,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public interface JpaclassesRepository extends CrudRepository<classes, Long> {
 
+    // retrieving class from schedule
     @Query("select c FROM classes c where c.class_ID = :id")
     classes scheduleclass(@Param("id") String id);
 
+    // remove student from class
     @Modifying
     @Query(value ="UPDATE classes SET students = REPLACE(students,:user,' '), slots_taken=slots_taken-1 WHERE class_id=:course", nativeQuery = true)
     void removeStudent(@Param("user") String user, @Param("course") String course);
 
+    // add user to class
     @Modifying
     @Query(value="UPDATE classes SET classes.students = CONCAT(CONCAT(students,:user),' '),slots_taken=slots_taken+1 WHERE classes.class_id=:course",nativeQuery = true)
     void addUser(@Param("user") String user, @Param("course") String course);
+
+    // editing a class
+    // admin can edit semester, capacity, professor
+    // using semester, capacity, professor as parameters
+    @Modifying
+    @Query(value="UPDATE classes SET classes.semester = :semester, classes.capacity=:capacity, classes.professor=:professor WHERE classes.class_id=:course ",nativeQuery = true)
+    void editClass(@Param("semester") String semester, @Param("capacity") int capacity, @Param("professor") String professor);
 }
